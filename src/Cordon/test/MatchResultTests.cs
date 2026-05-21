7e26c4d266e3305f0adcfed5568bc7f7d40867fe
MonkSoul <monksoul@outlook.com>
2026-05-21 11:23:13 +0800

    😊 完成敏感词模块代码编写

---
+++ src/Cordon/test/MatchResultTests.cs
@@ -0,0 +1,24 @@
+﻿// 版权归百小僧及百签科技（广东）有限公司所有。
+// 
+// 此源代码遵循位于源代码树根目录中的 LICENSE 文件的许可证。
+
+namespace Cordon.Tests;
+
+public class MatchResultTests
+{
+    [Fact]
+    public void New_ReturnOK()
+    {
+        var result = new MatchResult("敏感词", 1, 5);
+        Assert.Equal("敏感词", result.Word);
+        Assert.Equal(1, result.StartIndex);
+        Assert.Equal(5, result.EndIndex);
+    }
+
+    [Fact]
+    public void ToString_ReturnOK()
+    {
+        var result = new MatchResult("敏感词", 1, 5);
+        Assert.Equal("[敏感词] @ 1..5", result.ToString());
+    }
+}
---
+++ src/Cordon/test/TrieNodeTests.cs
@@ -0,0 +1,22 @@
+﻿// 版权归百小僧及百签科技（广东）有限公司所有。
+// 
+// 此源代码遵循位于源代码树根目录中的 LICENSE 文件的许可证。
+
+namespace Cordon.Tests;
+
+public class TrieNodeTests
+{
+    [Fact]
+    public void New_ReturnOK()
+    {
+        var node = new TrieNode { IsEnd = true };
+        node.Fail = node;
+        node.Children.Add('敏', new TrieNode { IsEnd = true });
+        node.MatchedWords.Add(("敏感词", 3));
+
+        Assert.NotNull(node.Fail);
+        Assert.True(node.IsEnd);
+        Assert.Single(node.Children);
+        Assert.Single(node.MatchedWords);
+    }
+}
